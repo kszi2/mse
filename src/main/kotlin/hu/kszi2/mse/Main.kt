d194ebe7f22e06129ed1f591a5f19c098137d539
Kovács Bertalan <berci9ke101@gmail.com>
2023-09-18 15:41:06 +0200

    feat: Basic schpincer

--- src/main/kotlin/hu/kszi2/mse/Main.kt
+++ src/main/kotlin/hu/kszi2/mse/Main.kt
@@ -2,7 +2,6 @@ package hu.kszi2.mse
 
 import hu.kszi2.mse.extension.ping.Ping
 import hu.kszi2.mse.extension.schpincer.SchPincer
-import hu.kszi2.mse.extension.schpincer.SchPincerEvent
 import hu.kszi2.mse.extension.statusch.Statusch
 import hu.kszi2.mse.registrable.registerExtension
 
@@ -10,9 +9,6 @@ import hu.kszi2.mse.registrable.registerExtension
  * This is where your bot is...
  */
 suspend fun main() {
-
-SchPincerEvent().apiParseBody().forEach{ println(it)}
-
     bot(BOT_TOKEN) {
         registerExtension(Statusch(), Ping(), SchPincer())
         println(createBotInvite().toString())
--- src/main/kotlin/hu/kszi2/mse/extension/schpincer/SchPincer.kt
+++ src/main/kotlin/hu/kszi2/mse/extension/schpincer/SchPincer.kt
@@ -9,23 +9,33 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.javacord.api.DiscordApi
+import org.javacord.api.entity.message.embed.EmbedBuilder
 import org.javacord.api.interaction.SlashCommand
 import org.javacord.api.interaction.SlashCommandInteraction
+import java.awt.Color
+import java.time.Clock
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import kotlinx.serialization.encodeToString
 
 class SchPincer : RegistrableExtension(SchPincerCommand(), SchPincerEvent())
 
-public class SchPincerEvent : RegistrableEvent {
+private class SchPincerEvent : RegistrableEvent {
 
     @Serializable
     data class Opening(
         @SerialName("circleName") val name: String,
-        @SerialName("nextOpeningDate") val epoch: Long
+        @SerialName("nextOpeningDate") val epoch: Long,
+        @SerialName("outOfStock") val negstock: Boolean
     ) {
         override operator fun equals(other: Any?): Boolean {
             if (other is Opening) {
@@ -37,7 +47,7 @@ public class SchPincerEvent : RegistrableEvent {
 
     private val json = Json { ignoreUnknownKeys = true }
 
-    public suspend fun apiParseBody(): Set<Opening> {
+    private suspend fun apiParseBody(): Set<Opening> {
         val now: String = apiGetBody(Url("https://schpincer.sch.bme.hu/api/items/now"))
         val tomorrow: String = apiGetBody(Url("https://schpincer.sch.bme.hu/api/items/tomorrow"))
 
@@ -62,11 +72,41 @@ public class SchPincerEvent : RegistrableEvent {
         return run { withTimeout(2000) { client.get(url).body() } }
     }
 
+    private fun parseDateTime(datetime: LocalDateTime) =
+        "**(${datetime.dayOfWeek})** " +
+                "${
+                    datetime.dayOfMonth.toString().padStart(2, '0')
+                }/${
+                    datetime.monthValue.toString().padStart(2, '0')
+                }/${datetime.year} ${
+                    datetime.hour.toString().padStart(2, '0')
+                }:${datetime.minute.toString().padStart(2, '0')}"
+
     override fun registerEvent(api: DiscordApi) {
         api.addSlashCommandCreateListener { event ->
             val interaction: SlashCommandInteraction = event.slashCommandInteraction
             if (interaction.fullCommandName == "opening") {
+                //fetch openings
+                val openings = runBlocking { apiParseBody() }
 
+                //creating embed
+                val embed = EmbedBuilder()
+                    .setColor(Color.decode("#FFCCEE"))
+                    .setTitle("Openings :fork_and_knife:")
+                    .setTimestamp(Instant.now(Clock.systemUTC()))
+                    .setUrl("https://schpincer.sch.bme.hu")
+
+                openings.forEach {
+                    embed.addField("", it.name, true)
+                    embed.addField("", parseDateTime(LocalDateTime.ofEpochSecond(it.epoch / 1000, 0, ZoneOffset.UTC)), true)
+                    embed.addField("", "")
+                }
+
+                interaction
+                    .createImmediateResponder()
+                    .setContent("")
+                    .addEmbed(embed)
+                    .respond()
             }
         }
     }

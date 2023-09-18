2afe2aa20bf48b256499f3d4898b6f6f3f82a11c
Kovács Bertalan <berci9ke101@gmail.com>
2023-09-18 10:12:51 +0200

    feat: basic sch getter

--- src/main/kotlin/hu/kszi2/mse/Main.kt
+++ src/main/kotlin/hu/kszi2/mse/Main.kt
@@ -2,15 +2,19 @@ package hu.kszi2.mse
 
 import hu.kszi2.mse.extension.ping.Ping
 import hu.kszi2.mse.extension.schpincer.SchPincer
+import hu.kszi2.mse.extension.schpincer.SchPincerEvent
 import hu.kszi2.mse.extension.statusch.Statusch
 import hu.kszi2.mse.registrable.registerExtension
+import kotlinx.coroutines.runBlocking
 
 /**
  * This is where your bot is...
  */
 fun main() {
-    bot(BOT_TOKEN) {
-        registerExtension(Statusch(), Ping(), SchPincer())
-        println(createBotInvite().toString())
-    }
+    SchPincerEvent().getCurrOpening().forEach { println(it.groups["name"]?.value) }
+
+//    bot(BOT_TOKEN) {
+//        registerExtension(Statusch(), Ping(), SchPincer())
+//        println(createBotInvite().toString())
+//    }
 }
--- src/main/kotlin/hu/kszi2/mse/extension/schpincer/SchPincer.kt
+++ src/main/kotlin/hu/kszi2/mse/extension/schpincer/SchPincer.kt
@@ -3,18 +3,65 @@ package hu.kszi2.mse.extension.schpincer
 import hu.kszi2.mse.registrable.RegistrableCommand
 import hu.kszi2.mse.registrable.RegistrableEvent
 import hu.kszi2.mse.registrable.RegistrableExtension
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.util.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 import org.javacord.api.DiscordApi
+import org.javacord.api.interaction.SlashCommand
+import org.javacord.api.interaction.SlashCommandInteraction
+import org.javacord.api.interaction.SlashCommandOption
 
 class SchPincer : RegistrableExtension(SchPincerCommand(), SchPincerEvent())
 
-private class SchPincerEvent : RegistrableEvent {
+public class SchPincerEvent : RegistrableEvent {
+
+    public fun getCurrOpening(): Sequence<MatchResult> {
+        var body: String?
+        runBlocking {
+            withTimeout(2000) {
+                body = apiGetBody(Url("https://schpincer.sch.bme.hu/api/items/now"))
+            }
+        }
+
+        if (body == null) {
+            throw NullPointerException("The request body was empty!")
+        }
+
+        //filter
+        //src: @bodand
+        return Regex("\"circleName\":\"(?<name>.+?)\"").findAll(body!!)
+    }
+
+    @OptIn(InternalAPI::class)
+    private suspend fun apiGetBody(url: Url): String {
+        val client = HttpClient(CIO) {
+            install(UserAgent) {
+                agent =
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0"
+            }
+        }
+
+        return client.get(url).body()
+    }
+
     override fun registerEvent(api: DiscordApi) {
-        TODO("Not yet implemented")
+        api.addSlashCommandCreateListener { event ->
+            val interaction: SlashCommandInteraction = event.slashCommandInteraction
+            if (interaction.fullCommandName == "opening") {
+
+            }
+        }
     }
 }
 
 private class SchPincerCommand : RegistrableCommand {
     override fun registerCommand(api: DiscordApi) {
-        TODO("Not yet implemented")
+        SlashCommand.with("opening", "Request SCH-PINCÉR openings.").createGlobal(api).join()
     }
 }

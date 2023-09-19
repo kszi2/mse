5ea7c46f407da75c6e4565d2bbd0e9d0ada3cb51
Kovács Bertalan <berci9ke101@gmail.com>
2023-09-19 11:39:25 +0200

    feat: kabal v0.1

--- src/main/kotlin/hu/kszi2/kabal/Kabal.kt
+++ src/main/kotlin/hu/kszi2/kabal/Kabal.kt
@@ -1,25 +1,43 @@
 package hu.kszi2.kabal
 
 import kotlinx.coroutines.*
-import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.run
 
-class Kabal(var interval: KabalInterval = KabalInterval.SECOND) {
-    suspend fun <R> krunonce(block: () -> R): R {
-        return run {
-            delay(this.interval.delay)
-            block.invoke()
+@OptIn(DelicateCoroutinesApi::class)
+class Kabal {
+    private var scope: GlobalScope = GlobalScope
+    var interval: KabalInterval = KabalInterval.SECOND
+        get() {
+            return field
         }
+        set(value) {
+            field = value
+        }
+
+    companion object {
+        val DEFAULT = Kabal()
     }
 
+    internal suspend fun <R> krunonce(block: () -> R): R {
+        val ret = scope.async {
+            delay(this@Kabal.interval.delay)
+            block()
+        }.await()
+        return ret
+    }
 
-    suspend fun krun(block: () -> Unit) {
-        run {
+    suspend fun <R> krun(block: () -> R): Job {
+        val job = scope.async {
             while (true) {
-                block.invoke()
-                delay(this.interval.delay)
+                block()
+                delay(this@Kabal.interval.delay)
             }
-        }
 
+        }
+        return job
     }
+}
+
+suspend fun <R> kabal(func: suspend Kabal.() -> R): R {
+    return run { Kabal.DEFAULT.func() }
 }
--- src/main/kotlin/hu/kszi2/mse/Main.kt
+++ src/main/kotlin/hu/kszi2/mse/Main.kt
@@ -1,16 +1,14 @@
 package hu.kszi2.mse
 
-import hu.kszi2.mse.extension.ping.*
-import hu.kszi2.mse.extension.schpincer.*
-import hu.kszi2.mse.extension.statusch.*
-import hu.kszi2.mse.registrable.*
+import hu.kszi2.kabal.KabalInterval
+import hu.kszi2.kabal.kabal
 
 /**
  * This is where your bot is...
  */
-fun main() {
-    bot(BOT_TOKEN) {
-        registerExtension(Statusch(), Ping(), SchPincer())
-        println(createBotInvite().toString())
-    }
+suspend fun main() {
+//    bot(BOT_TOKEN) {
+//        registerExtension(Statusch(), Ping(), SchPincer())
+//        println(createBotInvite().toString())
+//    }
 }

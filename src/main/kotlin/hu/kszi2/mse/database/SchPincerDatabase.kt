b4737df459333b65768ffb89c03ba169439cc618
Kovács Bertalan <berci9ke101@gmail.com>
2023-09-28 23:07:29 +0200

    feat: schdb documentation

--- src/main/kotlin/hu/kszi2/mse/database/SchPincerDatabase.kt
+++ src/main/kotlin/hu/kszi2/mse/database/SchPincerDatabase.kt
@@ -1,17 +1,24 @@
 package hu.kszi2.mse.database
 
+import hu.kszi2.mse.extension.schpincer.*
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 
+/**
+ * DAO for the [SchPincer] api
+ */
 object DBOpenings : IntIdTable() {
     val circleName = varchar("circlename", 50)
     val nextOpeningDate = datetime("nextopeningdate").index()
     val outOfStock = bool("outofstock")
 }
 
+/**
+ * Class backing the [DBOpenings] object
+ */
 class DBOpening(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<DBOpening>(DBOpenings)
 

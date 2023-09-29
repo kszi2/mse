5ebcae51e9fdad9b5908280229645a2a5c853278
Kovács Bertalan <berci9ke101@gmail.com>
2023-09-29 12:42:54 +0200

    feat: README.md

--- src/main/kotlin/hu/kszi2/mse/Bot.kt
+++ src/main/kotlin/hu/kszi2/mse/Bot.kt
@@ -1,7 +1,5 @@
 package hu.kszi2.mse
 
-import hu.kszi2.kontext.KontextInterval
-import hu.kszi2.kontext.kontext
 import org.javacord.api.*
 import org.javacord.api.entity.intent.Intent
 import java.io.File
